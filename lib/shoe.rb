373213feb1160cef7a856babc67d136f00a9e859
Corina Talavera <corina.talavera@gmail.com>
2018-11-11 21:39:06 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  BRANDS = []
+   if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
 
   def cobble
     self.condition = "new"
